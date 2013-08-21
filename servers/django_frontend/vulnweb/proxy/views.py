f80d97fe71a1bd93206e3b1930334579c46d5d56
Andres Riancho <andres.riancho@gmail.com>
2013-08-21 10:08:33 -0300

    Nicer error handling

--- servers/django_frontend/vulnweb/proxy/views.py
+++ servers/django_frontend/vulnweb/proxy/views.py
@@ -11,12 +11,16 @@ def proxy(request):
     http://127.0.0.1:8000/?url=http://httpbin.org/user-agent should return
     the python-requests user agent.
     '''
-    url = request.GET['url']
-    
     try:
-        response = requests.get(url)
-        text = response.text
-    except Exception, e:
-        text = 'HTTP request failed %s' % e
+        url = request.GET['url']
+    except:
+        text = 'The URL query string parameter is required.'\
+               '<a href="/?url=http://httpbin.org/user-agent">Example.</a>' 
+    else:
+        try:
+            response = requests.get(url)
+            text = response.text
+        except Exception, e:
+            text = 'HTTP request failed %s' % e
     
     return HttpResponse(text)
