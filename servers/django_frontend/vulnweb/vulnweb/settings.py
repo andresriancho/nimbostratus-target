7ccaa345c55ebe399b758993e3478684223cd539
Andres Riancho <andres.riancho@gmail.com>
2013-08-21 12:00:14 -0300

    Fix incorrect import

--- servers/django_frontend/vulnweb/vulnweb/settings.py
+++ servers/django_frontend/vulnweb/vulnweb/settings.py
@@ -154,15 +154,13 @@ LOGGING = {
     }
 }
 
-from config import REGION
-
 BROKER_TRANSPORT = 'sqs'
 BOTO_PROVIDER = 'boto-provider'
 BROKER_URL = '%s://%s:%s@' % (BROKER_TRANSPORT, BOTO_PROVIDER, None),
 CELERY_IGNORE_RESULT = True,
 BROKER_TRANSPORT_OPTIONS = {
                             'queue_name_prefix': 'nimbostratus-',
-                            'region': REGION,
+                            'region': 'ap-southeast-1',
 }
 
 import djcelery
