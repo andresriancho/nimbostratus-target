7d12aca2ad7fb278851a245a61b6eea11c363fe7
Andres Riancho <andres.riancho@gmail.com>
2015-03-05 11:37:03 -0300

    Changed so others can also clone

--- servers/django_frontend/user_data.py
+++ servers/django_frontend/user_data.py
@@ -14,7 +14,7 @@ from datetime import timedelta
 LOG_FILE = '/var/log/cloud-user-data.log'
 
 # Where to get the code from
-VULNWEB_REPO = 'git@github.com:andresriancho/nimbostratus-target.git'
+VULNWEB_REPO = 'https://github.com/andresriancho/nimbostratus-target.git'
 VULNWEB_BRANCH = 'master'
 
 NGINX_CONFIG = '''\
